5d20b2fc4a5f16ee21ab9f3806e592e3b78114fe
Martin Tovmassian <martin.tovmassian@protonmail.com>
2023-07-12 23:15:52 +0200

    :children_crossing: (global) more styling

---
+++ content/blog/2023-07-04-mes-lectures-en-2023.md
@@ -0,0 +1,11 @@
+---
+title: "Mes lectures en 2023"
+description: ""
+date: 2023-07-03T22:25:57.147Z
+preview: ""
+draft: false
+tags: []
+categories: []
+image: https://fakeimg.pl/350x350/?text=Tiva&font=abel
+---
+![](https://fakeimg.pl/350x350/?text=Tiva&font=abel)
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,16 @@ module.exports = {
           400: "#9CA3AF",
           700: "#374151",
           800: "#1F2937"
+        },
+        tivagreen: {
+          600: "rgb(64,143,145)",
+          800: "rgb(6,88,85)",
+          900: "rgb(9,78,78)",
         }
       }
+    },
+    fontFamily: {
+      tiva: ["Abel"]
     }
   },
   variants: {},
